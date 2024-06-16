1caf19ba581b099d847ccb37a690ac0d5defacdc
Nathan Bailey <nathanbaileyw@gmail.com>
2024-06-16 13:30:38 +0100

    Add diffusion models

--- diffusion/part1.py
+++ diffusion/part1.py
@@ -114,7 +114,7 @@ def ResidualBlock(width):
         else:
             residual = layers.Conv2D(width, kernel_size=1)(x)
         x = layers.BatchNormalization(center=False, scale=False)(x)
-        x = layers.Conv2D(width, kernel_size=3, padding="same", activations=activations.swish)(x)
+        x = layers.Conv2D(width, kernel_size=3, padding="same", activation=activations.swish)(x)
         x = layers.Conv2D(width, kernel_size=3, padding="same")(x)
         x = layers.Add()([x, residual])
         return x
@@ -123,4 +123,154 @@ def ResidualBlock(width):
 def DownBlock(width, block_depth):
     def apply(x):
         x, skips = x
+        for _ in range(block_depth):
+            x = ResidualBlock(width)(x)
+            skips.append(x)
+        x = layers.AveragePooling2D(pool_size=2)(x)
+        return x
+    return apply
+
+def UpBlock(width, block_depth):
+    def apply(x):
+        x, skips = x
+        x = layers.UpSampling2D(size=2, interpolation="bilinear")(x)
+        for _ in range(block_depth):
+            x = layers.Concatenate()([x, skips.pop()])
+            x = ResidualBlock(width)(x)
+        return x
+    return apply
+
+noisy_images = layers.Input(shape=(IMAGE_SIZE, IMAGE_SIZE, 3))
+x = layers.Conv2D(32, kernel_size=1)(noisy_images)
+
+noise_variances = layers.Input(shape=(1,1,1))
+noise_embedding = layers.Lambda(sinusoidal_embedding)(noise_variances)
+noise_embedding = layers.UpSampling2D(size=IMAGE_SIZE, interpolation="nearest")(noise_embedding)
+
+x = layers.Concatenate()([x, noise_embedding])
+
+skips = []
+
+x = DownBlock(32, block_depth=2)([x, skips])
+x = DownBlock(64, block_depth=2)([x, skips])
+x = DownBlock(96, block_depth=2)([x, skips])
+
+x = ResidualBlock(128)(x)
+x = ResidualBlock(128)(x)
+
+x = UpBlock(96, block_depth=2)([x, skips])
+x = UpBlock(64, block_depth=2)([x, skips])
+x = UpBlock(32, block_depth=2)([x, skips])
+
+x = layers.Conv2D(3, kernel_size=1, kernel_initializer="zeros")(x)
+
+unet = models.Model([noisy_images, noise_variances], x, name="unet")
+
+class DiffusionModel(models.Model):
+    def __init__(self):
+        super().__init__()
+        self.normalizer = layers.Normalization()
+        self.network = unet
+        self.ema_network = models.clone_model(self.network)
+        self.diffusion_schedule = offset_cosine_diffusion_schedule
+        self.noise_loss_tracker = metrics.Mean(name="n_loss")
+    
+    # def compile(self, **kwargs):
+    #     super().compile(**kwargs)
+    #     self.noise_loss_tracker = metrics.Mean(name="n_loss")
+    
+    @property
+    def metrics(self):
+        return [self.noise_loss_tracker]
+
+    def denormalize(self, images):
+        images = self.normalizer.mean + images * self.normalizer.variance**0.5
+        return tf.clip_by_value(images, 0.0, 1.0)
+    
+    def denoise(self, noisy_images, noise_rates, signal_rates, training):
+        if training:
+            network = self.network
+        else:
+            network = self.ema_network
         
+        pred_noises = network([noisy_images, noise_rates**2], training=training)
+        pred_images = (noisy_images - noise_rates*pred_noises) / signal_rates
+        return pred_noises, pred_images
+
+    
+    def reverse_diffusion(self, initial_noise, diffusion_steps):
+        num_images = initial_noise.shape[0]
+        step_size = 1.0 / diffusion_steps
+        current_images = initial_noise
+        for step in range(diffusion_steps):
+            #Start from 1 and work backwards
+            #Assume we are at x (t) - final image so t = 1
+            diffusion_times = tf.ones((num_images, 1, 1, 1)) - step*step_size
+            noise_rates, signal_rates = self.diffusion_schedule(diffusion_times)
+            pred_noises, pred_images = self.denoise(current_images, noise_rates, signal_rates, training=False)
+            next_diffusion_times = diffusion_times - step_size
+            next_noise_rates, next_signal_rates = self.diffusion_schedule(next_diffusion_times)
+            current_images = (next_signal_rates * pred_images + next_noise_rates * pred_noises)
+        return pred_images
+    
+    def generate(self, num_images, diffusion_steps, initial_noise=None):
+        if initial_noise is None:
+            initial_noise = tf.random.normal(shape=(num_images, IMAGE_SIZE, IMAGE_SIZE, 3))
+        generated_images = self.reverse_diffusion(initial_noise, diffusion_steps)
+        generated_images = self.denormalize(generated_images)
+        return generated_images
+
+    
+    def train_step(self, images):
+        images = self.normalizer(images, training=True)
+        noises = tf.random.normal(shape=(BATCH_SIZE, IMAGE_SIZE, IMAGE_SIZE, 3))
+        # we are generating an image x(t) at a random timestep t
+        diffusion_times = tf.random.uniform(shape=(BATCH_SIZE, 1, 1, 1,), minval=0.0, maxval=1.0)
+        noise_rates, signal_rates = self.diffusion_schedule(diffusion_times)
+
+        noisy_images = signal_rates * images + noise_rates * noises
+
+        with tf.GradientTape() as tape:
+            pred_noises, _ = self.denoise(noisy_images, noise_rates, signal_rates, training=True)
+            noise_loss = self.loss(noises, pred_noises)
+    
+        gradients = tape.gradient(noise_loss, self.network.trainable_weights)
+        self.optimizer.apply_gradients(zip(gradients, self.network.trainable_weights))
+        self.noise_loss_tracker.update_state(noise_loss)
+
+        for weight, ema_weight in zip(self.network.weights, self.ema_network.weights):
+            ema_weight.assign(EMA*ema_weight+(1-EMA)*weight)
+        
+        return {m.name: m.result() for m in self.metrics}
+
+    def test_step(self, images):
+        images = self.normalizer(images, training=False)
+        noises = tf.random.normal(shape=(BATCH_SIZE, IMAGE_SIZE, IMAGE_SIZE, 3))
+        diffusion_times = tf.random.uniform(shape=(BATCH_SIZE, 1, 1, 1,), minval=0.0, maxval=1.0)
+        noise_rates, signal_rates = self.diffusion_schedule(diffusion_times)
+        noisy_images = signal_rates * images + noise_rates * noises
+        pred_noises, _ = self.denoise(noisy_images, noise_rates, signal_rates, training=True)
+        noise_loss = self.loss(noises, pred_noises)
+        self.noise_loss_tracker.update_state(noise_loss)
+        return {m.name: m.result() for m in self.metrics}
+
+
+    
+
+model = DiffusionModel()
+model.normalizer.adapt(train)
+model.compile(optimizer=optimizers.AdamW(learning_rate=LEARNING_RATE, weight_decay=WEIGHT_DECAY), loss=losses.mean_absolute_error)
+class ImageGenerator(callbacks.Callback):
+    def __init__(self, num_img):
+        self.num_img = num_img
+    
+    def on_epoch_end(self, epoch, logs=None):
+        generated_images = self.model.generate(num_images=self.num_img, diffusion_steps=PLOT_DIFFUSION_STEPS).numpy()
+        display(generated_images, save_to="./output/generated_img_%03d.png" % (epoch))
+
+image_generator_callback = ImageGenerator(num_img=10)
+
+model.fit(train, epochs=EPOCHS, callbacks=[image_generator_callback], verbose=2)
+
+generated_images = model.generate(num_images=10, diffusion_steps=PLOT_DIFFUSION_STEPS).numpy()
+display(generated_images, save_to="./output/final_images.png")
